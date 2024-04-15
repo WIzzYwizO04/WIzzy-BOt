ec6dfd43cf41be613191e341d6d301c5e714a794
WIzzYwizO04 <150206106+WIzzYwizO04@users.noreply.github.com>
2024-04-15 12:45:09 +0100

    Wizzy.js
    ..

--- Wizzy.js
+++ Wizzy.js
@@ -1,6 +1,6 @@
 /**
  * @author WIzzyWIzo
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! The source code is written by WIzzyWIzo, please don't change the author's name everywhere. Thank you for using
  * ! Official source code: https://github.com/WIzzYwizO04/WIzzy-BOt
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
  *
